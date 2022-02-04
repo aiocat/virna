1617c87ebe96d02842a0f85d41cc2afa2c7e310c
aiocat <aiocat@protonmail.com>
2022-02-04 18:09:23 +0300

    delete c++ file after compiling

--- example/brainfuck/hw.c
+++ example/brainfuck/hw.c
@@ -1,3 +1,3 @@
 #include <stdio.h>
-char array[30000]={0}; char *ptr = array;
+char array[30000]={0};char *ptr=array;
 int main(){++*ptr;++*ptr;++*ptr;++*ptr;++*ptr;++*ptr;++*ptr;++*ptr;while(*ptr){++ptr;++*ptr;++*ptr;++*ptr;++*ptr;while(*ptr){++ptr;++*ptr;++*ptr;++ptr;++*ptr;++*ptr;++*ptr;++ptr;++*ptr;++*ptr;++*ptr;++ptr;++*ptr;--ptr;--ptr;--ptr;--ptr;--*ptr;};++ptr;++*ptr;++ptr;++*ptr;++ptr;--*ptr;++ptr;++ptr;++*ptr;while(*ptr){--ptr;};--ptr;--*ptr;};++ptr;++ptr;putchar(*ptr);++ptr;--*ptr;--*ptr;--*ptr;putchar(*ptr);++*ptr;++*ptr;++*ptr;++*ptr;++*ptr;++*ptr;++*ptr;putchar(*ptr);putchar(*ptr);++*ptr;++*ptr;++*ptr;putchar(*ptr);++ptr;++ptr;putchar(*ptr);--ptr;--*ptr;putchar(*ptr);--ptr;putchar(*ptr);++*ptr;++*ptr;++*ptr;putchar(*ptr);--*ptr;--*ptr;--*ptr;--*ptr;--*ptr;--*ptr;putchar(*ptr);--*ptr;--*ptr;--*ptr;--*ptr;--*ptr;--*ptr;--*ptr;--*ptr;putchar(*ptr);++ptr;++ptr;++*ptr;putchar(*ptr);++ptr;++*ptr;++*ptr;putchar(*ptr);};
